9d251c7d36d858280c9465ba7e5236276861a940
Charles Damasceno <swirfneblin@gmail.com>
2023-08-10 00:54:12 -0300

    built docker image for app

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# Stage 1: Build the Golang application binary
+FROM arm64v8/golang:1.19-buster AS builder
+WORKDIR /go/src/app
+COPY ./app /go/src/app
+RUN go build -o myapp
+
+# Stage 2: Create a minimal runtime image
+FROM arm64v8/alpine:latest
+WORKDIR /app
+COPY --from=builder /go/src/app/myapp /app/
+CMD ["./myapp"]
--- sql/init.sql
+++ sql/init.sql
@@ -2,9 +2,9 @@ CREATE DATABASE IF NOT EXISTS `devbook` DEFAULT CHARACTER SET utf8 COLLATE utf8_
 
 USE `devbook`;
 
-DROP TABLE IF EXISTS `usuarios`;
-DROP TABLE IF EXISTS `seguidores`;
 DROP TABLE IF EXISTS `publicacoes`;
+DROP TABLE IF EXISTS `seguidores`;
+DROP TABLE IF EXISTS `usuarios`;
 
 CREATE TABLE usuarios(
   id int AUTO_INCREMENT primary key,
@@ -41,4 +41,25 @@ CREATE TABLE publicacoes(
   curtidas int default 0,
   criadaEm timestamp default current_timestamp()
 ) ENGINE=InnoDB;
-  
+
+delete from usuarios;
+delete from seguidores;
+delete from publicacoes;
+
+INSERT INTO usuarios (nome, nick, email, senha) 
+VALUES 
+('Usuário 1', 'usuario_1', 'usuario1@gmail.com', 'ogQxt96qXLaa3YO73QpMSsY6MjmU3RiSeuKOfoo4LMSfL2gGGUk7S6y2LRJVblI+z5JAk1SFpP2TMYOfXJ+1Eg=='),
+('Usuário 2', 'usuario_2', 'usuario2@gmail.com', 'ogQxt96qXLaa3YO73QpMSsY6MjmU3RiSeuKOfoo4LMSfL2gGGUk7S6y2LRJVblI+z5JAk1SFpP2TMYOfXJ+1Eg=='),
+('Usuário 3', 'usuario_3', 'usuario3@gmail.com', 'ogQxt96qXLaa3YO73QpMSsY6MjmU3RiSeuKOfoo4LMSfL2gGGUk7S6y2LRJVblI+z5JAk1SFpP2TMYOfXJ+1Eg==');
+
+INSERT INTO seguidores (usuario_id, seguidor_id)
+VALUES
+(1,2),
+(3,1),
+(1,3);
+
+insert into publicacoes(titulo, conteudo, autor_id)
+VALUES
+('publicacao do usuario 1', 'Essa é a publicacao do usuario 1', 1),
+('publicacao do usuario 2', 'Essa é a publicacao do usuario 2', 2),
+('publicacao do usuario 3', 'Essa é a publicacao do usuario 3', 3);
